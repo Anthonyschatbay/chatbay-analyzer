c75d2839f5763850d56e09ba5ac2d25ce84b37a9
Anthonyschatbay <anthonysauctions@outlook.com>
2025-10-21 05:51:24 -0400

    feat: v5.5 full header + attachment CSV

--- restrict-wp-uploads.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_filter('upload_dir', function ($dirs) {
-    // Force WP uploads to stay in wp-content/uploads
-    $dirs['subdir']  = ''; 
-    $dirs['path']    = WP_CONTENT_DIR . '/uploads';
-    $dirs['url']     = content_url('/uploads');
-    return $dirs;
-});
